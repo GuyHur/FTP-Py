36c6377b0a9de7fe71db8bce67fefc72233d0b3a
Guy Hrushvski <guyhrushovski@gmail.com>
2021-04-06 19:56:11 +0300

    Redo

--- README.md
+++ README.md
@@ -1,28 +1,2 @@
-# FTPGuy Version 1
+# FTPGuy - FTP Client & Server in Python
 
-FTP implementation in python
-
-
-  # Features:
-  Basic RSA encryption
-  Basic file transfer
-  Thread for each connected client
-  
-  # Installation
-  clone this repo
-  pip install -r requirements.txt
-  
-
-# FTPGuy Version 2
-
-  # Features
-  Multithreaded server
-  More commands
-  TLS Encryption
-  GUI For filesearch
-  Better Error handling
-  
-  # Installation
-  
-  Clone this repo
-  
--- Server.py
+++ Server.py
@@ -1,167 +1,62 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
 import socket
-from cryptography.hazmat.primitives.serialization import load_pem_private_key
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives import serialization
+import threading
 import os
 import sys
-import threading
-class FTPServer():
-    """
-    FTPServer
-    Server for the FTP program: http://github.com/GuyHur/FTP
-    """
+import time
 
-    class Client():
-        """
-        Client class for inventory keep
-        """
+class Server(threading.Thread):
+    def __init__(self, sock, addr, port):
+        threading.Thread.__init__(self)
+        self.auth = False
+        self.sock = sock
+        self.addr = addr
+        self.port = port
+        self.cwd = "/home/Desktop"
 
-        def __init__(self, socket, address, name):
-            self.s = socket
-            self.address = address
-            self.name = name
-
-        def __str__(self):
-            return 'Client id: {id}, Client address: {address}'.format(id=self.id, address=self.address)
-
-    def __init__(self):
-        """
-        init server
-        :var private_key - server private key[DO NOT SEND THE CLIENT]
-        :var public_key - send to the client to encrypt the file
-        :var string host- server ip
-        :var int server_port - server port, initializes at 5001
-        :var int buffer - file buffer
-        :var connections[] - array of Clients
-        :var socket- socket
-        :var client - client class
-        """
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.private_key = None
-        self.public_key = None
-        self.ThreadCount = 0
-        self.host = "0.0.0.0"
-        self.server_port = 5001
-        self.buffer = 4096
-        self.encrypted_file = None
-        self.connections = []
-        self.path = "Desktop/FTP/"
-        self.client = self.Client(None, None, None)
-
-    def handle_client(self, client_socket, client_address):
-        print("[NEW CONNECTION]" + str(client_address) + "connected.")
-        self.send_public_key(client_socket, client_address)
-        print("[+] Sent public key to client.")
-        with open('recieved_file_encrypted', 'wb') as encrypted_file:
-
-            data = True
-            while data:
-                data = self.s.recv(self.buffer)
-
-                if not data:
-                    print("Finished receiving file.")
+    
+    def run(self):
+        while True:
+            try:
+                data = self.sock.recv(1024).rstrip()
+                try:
+                    command = data.decode('utf-8')
+                except AttributeError:
+                    command = data
+                
+                if not command:
                     break
-            encrypted_file.write(data)
+            except socket.error as err:
+                print(err)
 
-
-    def send_public_key(self, client_socket, client_address):
-        print("[+]Sending public key to client at :" + self.client.address)
-        with open("public_key.pem", "rb") as public_key:
-            data = public_key.read(self.buffer)
-            while data:
-                client_socket.send(data)
-                print("Sent", repr(data))
-
-
-
-
-    def decrypt_file(self):
-        """
-        decrypts the file with the private key in private_key.pm
-        """
-        original_file = self.private_key.decrypt(
-                    encrypted,
-                    padding.OAEP(
-                        mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                        algorithm=hashes.SHA256(),
-                        label=None
-                    )
-                )
-        with open('decrypted_file', 'wb') as decrypted:
-            decrypted.write(original_file)
-
-    def start(self):
-        self.init()
-        try:
-            while True:
-                client_socket, client_address = self.s.accept()
-                thread = threading.Thread(target=self.handle_client(), args=(client_socket, client_address) )
-                thread.start()
-
-                self.send_public_key()
-                self.receive_encrypted_file()
-                self.decrypt_file()
-                self.s.close()
-
-        except Exception as e:
-            print(e)
-
+            try:
+                command, arg = command[:4].strip().upper(), command[4:].strip( ) or None
+                func = getattr(self, command)
+            except AttributeError as err:
+                print("Command not found.")
     def init(self):
-        """
-        Server starts here!!!
-        Initialize everything on the program
-        -Server initializes
-        """
+        print("Starting the server socket...")
         try:
-            self.generate_keypairs()
-            self.save_keypairs()
-            print("[+]Generated keys...")
-            # Creates Public & Private RSA keys and save them to self.public_key and self.private_key
-            self.s.bind((self.host, self.server_port))
-            print("Binding " + self.host + "to " + str(self.server_port))
-            self.s.listen()
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.addr, self.port))
+        
+        except socket.error as err:
+            print("Error starting socket. " + err)
 
-        except Exception as exc:
-            print(str(exc))
+    def sendCommand(self, command):
+        self.sock.send(command.encode('utf-8'))
 
-    def generate_keypairs(self):
-        private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
-        public_key = private_key.public_key()
-        self.private_key = private_key
-        self.public_key = public_key
+    def LIST(self, dirpath):
+        if not self.auth:
+            self.sendCommand("[-]User not logged in")
+            return
 
-    def save_keypairs(self):
-        try:
-            if self.private_key == None or self.public_key == None:
-                print("Keys are not valid to save..")
-            pem = self.private_key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption()
-            )
-            with open('private_key.pem', 'wb') as f:
-                f.write(pem)
-            pem = self.public_key.public_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PublicFormat.SubjectPublicKeyInfo
-            )
-            with open('public_key.pem', 'wb') as f:
-                f.write(pem)
+        if not dirpath:
+            # List current directory
+            pathname = os.path.abspath(os.path.join(self.cwd, '.'))
 
-        except Exception as e:
-            print(e)
+        elif dirpath.startswith(os.path.sep):
+            pathname = os.path.abspath(dirpath)
+        else:
+            pathname = os.path.abspath(os.path.join(self.cwd, dirpath))
 
 
-def main():
-    """
-    starting function
-    Runs server
-    """
-    server = FTPServer()
-    server.start()
-
-
-if __name__ == '__main__':
-    main()
